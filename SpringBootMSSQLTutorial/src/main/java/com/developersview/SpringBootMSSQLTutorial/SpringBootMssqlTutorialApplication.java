1789d305b0b59ecfbe4af9b67e066f88c239feae
developersview <pcslg1998@gmail.com>
2022-07-12 15:50:54 +0530

    initial commit

---
+++ SpringBootMSSQLTutorial/src/main/java/com/developersview/SpringBootMSSQLTutorial/SpringBootMssqlTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.developersview.SpringBootMSSQLTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMssqlTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMssqlTutorialApplication.class, args);
+	}
+
+}
---
+++ SpringBootMSSQLTutorial/src/test/java/com/developersview/SpringBootMSSQLTutorial/SpringBootMssqlTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developersview.SpringBootMSSQLTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMssqlTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
